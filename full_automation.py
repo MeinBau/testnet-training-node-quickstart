0eec934c85e2ab6b3c87c7220db2fcb1eb11dc58
Jun13KU <kplcfo4@gmail.com>
2025-07-31 17:24:08 +0900

    Update full_automation.py

--- full_automation.py
+++ full_automation.py
@@ -41,6 +41,14 @@ if __name__ == "__main__":
 
     # train all feasible models and merge
     model_set=["Qwen/Qwen2.5-7B-Instruct"]
+    training_args=LoraTrainingArguments(
+        num_train_epochs=3,
+        per_device_train_batch_size=2,
+        gradient_accumulation_steps=2,
+        lora_rank=32,
+        lora_alpha=16,
+        lora_dropout=0.05,
+    )
     for model_id in model_set:
         logger.info(f"Start to train the model {model_id}...")
         # if OOM, proceed to the next model
@@ -48,7 +56,7 @@ if __name__ == "__main__":
             train_lora(
                 model_id=model_id,
                 context_length=context_length,
-                training_args=LoraTrainingArguments(**all_training_args[model_id]),
+                training_args=training_args,
             )
         except RuntimeError as e:
             logger.error(f"Error: {e}")
