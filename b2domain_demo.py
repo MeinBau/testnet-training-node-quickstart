51ac666a63b1e65e769a47f2097390ebaceb2912
Jun13KU <kplcfo4@gmail.com>
2025-08-01 03:34:30 +0900

    Update b2domain_demo.py

--- b2domain_demo.py
+++ b2domain_demo.py
@@ -5,10 +5,14 @@ import torch
 from peft import LoraConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from trl import SFTTrainer, SFTConfig
+from loguru import logger
+from huggingface_hub import HfApi
 
 from dataset import TextDataset, SFTDataCollator
 from utils.constants import model2template
+from utils.gpu_utils import get_gpu_type
 
+HF_USERNAME = os.environ["HF_USERNAME"]
 
 @dataclass
 class LoraTrainingArguments:
@@ -140,3 +144,43 @@ if __name__ == "__main__":
     train_lora(     
         model_id=model_id, context_length=context_length, training_args=training_args
     )
+
+    gpu_type = get_gpu_type()
+
+    try:
+        logger.info("Start to push the lora weight to the hub...")
+        api = HfApi(token=os.environ["HF_TOKEN"])
+        repo_name = f"{HF_USERNAME}/domain-{model_id.replace('/', '-')}"            # check whether the repo exists
+        try:
+             api.create_repo(
+                repo_name,
+                exist_ok=False, 
+                repo_type="model",
+             )
+        except Exception:
+            logger.info(
+                f"Repo {repo_name} already exists. Will commit the new version."
+            )
+
+        commit_message = api.upload_folder(
+            folder_path="outputs",
+            repo_id=repo_name,
+            repo_type="model",
+        )
+        # get commit hash
+        commit_hash = commit_message.oid
+        logger.info(f"Commit hash: {commit_hash}")
+        logger.info(f"Repo name: {repo_name}")
+            # submit
+            # submit_task(
+            #     task_id, repo_name, "qwen1.5", gpu_type, commit_hash
+            # )
+            
+    except Exception as e:
+            logger.error(f"Error: {e}")
+            logger.info("Proceed to the next model...")
+    finally:
+            # cleanup merged_model and output
+            os.system("rm -rf merged_model")
+            os.system("rm -rf outputs")
+
