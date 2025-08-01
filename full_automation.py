41edc6a955ab010495dc10b1c06be05a22213a90
Jun13KU <kplcfo4@gmail.com>
2025-08-01 20:49:38 +0900

    프롬프트 수정

--- full_automation.py
+++ full_automation.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     # train all feasible models and merge
 
     training_args=LoraTrainingArguments(
-        num_train_epochs=1,
+        num_train_epochs=3,
         per_device_train_batch_size=1,
         gradient_accumulation_steps=2,
         lora_rank=16,
@@ -50,7 +50,7 @@ if __name__ == "__main__":
         lora_dropout=0.05,
     )
     ban_model_id_set= ['Qwen/Qwen1.5-0.5B']
-    model_id_set= ['']
+    model_id_set= ['Jun13KU/domain-Qwen-Qwen2.5-7B-Instruct']
 
     for model_id in model_id_set:
         logger.info(f"Start to train the model {model_id}...")
